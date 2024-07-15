6d08947d25d09cd26d6be4f64c98b1b71c582821
DanilAndreev <danssg08@gmail.com>
2024-07-15 17:00:42 +0300

    [Test] prebuild WIP

---
+++ Test/prebuild.py
@@ -0,0 +1,48 @@
+import os
+import sys as sys
+from os import path
+from argparse import ArgumentParser
+from subprocess import Popen, PIPE
+
+API_TO_LANG = {
+    "D3D12": "DXIL",
+    "Vulkan": "SPIRV",
+    "Metal": "MetalLib"
+}
+
+
+class PSOCompileHelper:
+    def __init__(self, scar_executable: str, api: str):
+        self.scar_executable = scar_executable
+        self.lang = API_TO_LANG[api]
+
+    def compile(self, desc_filepath: str, out_filepath: str):
+        if path.dirname(out_filepath):
+            os.makedirs(path.dirname(out_filepath), exist_ok=True)
+
+        compile_args = [desc_filepath]
+        compile_args += ["-o", out_filepath]
+        compile_args += ["--O0"]
+        compile_args += ["-t", self.lang]
+
+        process = Popen([self.scar_executable] + compile_args, stdout=PIPE, stderr=PIPE)
+        output, error = process.communicate()
+        process.wait()
+        if process.returncode != 0:
+            raise Exception(f"Failed to compile PSO. Compiler exited with error:\n{error.decode()}")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--compiler-path", required=True)
+    parser.add_argument("--api", required=True, choices=["D3D12", "Vulkan", "Metal"])
+    parser.add_argument("--example-id", required=True, choices=["RT", "Compute"])
+
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        exit(-1)
+
+    compileHelper = PSOCompileHelper(path.abspath(args.compiler_path), args.api)
+    compileHelper.compile("rt.desc.json", "testOut.scar")
