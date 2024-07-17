4c9d1c810516d578d341a90f55e3baef04510003
DanilAndreev <danssg08@gmail.com>
2024-07-18 02:02:52 +0300

    [Test] compute with dynamic descriptors

--- Test/shaders/compute/compute.hlsl
+++ Test/shaders/compute/compute.hlsl
@@ -1,10 +1,9 @@
-RWStructuredBuffer<int> DestUAV1 : register(u0, space0);
-RWStructuredBuffer<int> DestUAV2 : register(u1, space0);
+RWStructuredBuffer<int> DestUAV12[] : register(u0, space0);
 RWStructuredBuffer<int> DestUAV3 : register(u1, space1);
 
 [numthreads(64, 1, 1)]
 void main(uint3 DTid: SV_DispatchThreadID) {
-    DestUAV1[DTid.x] = int(DTid.x);
-    DestUAV2[DTid.x] = 1000 - int(DTid.x);
+    DestUAV12[0][DTid.x] = int(DTid.x);
+    DestUAV12[1][DTid.x] = 1000 - int(DTid.x);
     DestUAV3[DTid.x] = -int(DTid.x);
 }
